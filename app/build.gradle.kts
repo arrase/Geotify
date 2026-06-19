3953a9e560a22c01d3ae9944c339acf9d4362136
Juan Ezquerro LLanes <arrase@gmail.com>
2026-06-20 01:04:42 +0200

    Bump versionCode to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,8 +48,8 @@ android {
         applicationId = "dev.arrase.geotify"
         minSdk = 36
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.0-alpha.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
