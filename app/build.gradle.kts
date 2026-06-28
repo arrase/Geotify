9fbed8a3d9c4c2cd21b4abc072fe1d49f8caca43
Juan Ezquerro LLanes <arrase@gmail.com>
2026-06-28 21:55:37 +0200

    chore: bump version code to 8 and version name to 1.0.0-alpha.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,8 +48,8 @@ android {
         applicationId = "dev.arrase.geotify"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.0.0-alpha.7"
+        versionCode = 8
+        versionName = "1.0.0-alpha.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
