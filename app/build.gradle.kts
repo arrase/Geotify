f5186884a957f02317bd8d22deab6306e4497f3e
Juan Ezquerro LLanes <arrase@gmail.com>
2026-07-19 00:50:27 +0200

    ci: fix play store publish workflow for rejected app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,8 +48,8 @@ android {
         applicationId = "dev.arrase.geotify"
         minSdk = 24
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.0.0-beta.3"
+        versionCode = 11
+        versionName = "1.0.0-beta.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
